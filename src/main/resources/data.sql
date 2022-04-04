17d72e601ad517f25fe0c319150bb62e84c13483
ahyaemon <iamahyaemon@gmail.com>
2022-04-04 22:54:38 +0900

    実際のテーブルに即したテストデータを作成、サンプルは消去

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,42 @@
-insert into dog values (1, 'taro'), (2, 'jiro');
+insert into teacher values
+(1),
+(2),
+(3)
+;
+
+insert into student values
+(1, 'student_1', 'taro'),
+(2, 'student_2', 'jiro'),
+(3, 'student_3', 'saburo'),
+(4, 'student_4', 'siro'),
+(5, 'student_5', 'goro')
+;
+
+insert into classroom values
+(1, '上'),
+(2, '中'),
+(3, '下')
+;
+
+insert into student_belong_classroom values
+(1, 1),
+(2, 1),
+(3, 1),
+(1, 2),
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 2),
+(3, 3),
+(4, 3),
+(5, 3)
+;
+
+insert into teacher_belong_classroom values
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(3, 1)
+;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,24 @@
-create table dog(
-    id integer,
-    name text
+create table teacher(
+    id integer primary key
 );
+
+create table student(
+    id integer primary key,
+    login_id text not null,
+    name text not null
+);
+
+create table classroom(
+    id integer primary key,
+    name text not null
+);
+
+create table student_belong_classroom(
+    student_id integer not null references student(id),
+    classroom_id integer not null references classroom(id)
+);
+
+create table teacher_belong_classroom(
+    teacher_id integer not null references teacher(id),
+    classroom_id integer not null references classroom(id)
+)
