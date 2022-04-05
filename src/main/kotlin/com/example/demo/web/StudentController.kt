43d541c36587b1970d38550647904fa9d0ecd411
ahyaemon <iamahyaemon@gmail.com>
2022-04-05 23:04:41 +0900

    Controller のテスト追加とバグ取り

--- src/main/kotlin/com/example/demo/web/StudentController.kt
+++ src/main/kotlin/com/example/demo/web/StudentController.kt
@@ -2,8 +2,10 @@ package com.example.demo.web
 
 import com.example.demo.application.ListStudentsUseCase
 import com.example.demo.domain.vo.ValidationException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,6 +20,7 @@ class StudentController(
   }
 
   @ExceptionHandler(ValidationException::class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   fun handleValidationException(e: ValidationException): String {
     return e.message ?: "不明なエラーが発生しました"
   }
